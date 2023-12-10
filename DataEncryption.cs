a0674f5be0cb90e2d4952656db97a33d0bb57382
NojusKaubojus <123569125+NojusKaubojus@users.noreply.github.com>
2023-12-10 21:02:07 +0200

    Add files via upload

---
+++ DataEncryption.cs
@@ -0,0 +1,33 @@
+﻿public class DataEncryption
+{
+    private SymmetricAlgorithm algorithm;
+
+    public DataEncryption(SymmetricAlgorithm algorithm)
+    {
+        this.algorithm = algorithm;
+
+        // Įterpkite papildomą konfigūraciją pagal poreikį
+        algorithm.KeySize = 256;
+        algorithm.Mode = CipherMode.CFB;
+    }
+
+    public byte[] Encrypt(byte[] data, string password)
+    {
+        try
+        {
+            // Implementuokite šifravimo logiką naudodami 'algorithm'
+            // ...
+
+            byte[] encryptedData = null; // Pakeiskite į šifravimo rezultatą
+
+            return encryptedData;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Encryption error: {ex.Message}");
+            throw; // Persiųsti išimtį į aukštesnį lygį
+        }
+    }
+
+    // Papildomai įtraukite Decrypt metodą, jei jis dar nėra
+}
---
+++ NojusPS22/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+public partial class MainWindow : Window
+{
+    private DataEncryption dataEncryption;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        dataEncryption = new DataEncryption(new AesManaged());
+    }
+
+    private void BrowseButton_Click(object sender, RoutedEventArgs e)
+    {
+        var openFileDialog = new OpenFileDialog();
+        if (openFileDialog.ShowDialog() == true)
+        {
+            FilePathTextBox.Text = openFileDialog.FileName;
+        }
+    }
+
+    private async void EncryptButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            string password = PasswordBox.Password;
+            string filePath = FilePathTextBox.Text;
+
+            // Patikrinimas, ar slaptažodis ir kelias nėra tuščias
+            if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Please enter both password and file path.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            byte[] fileContent = await Task.Run(() => File.ReadAllBytes(filePath));
+            byte[] encryptedData = await Task.Run(() => dataEncryption.Encrypt(fileContent, password));
+
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Encrypted Files (*.enc)|*.enc";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                await Task.Run(() => File.WriteAllBytes(saveFileDialog.FileName, encryptedData));
+                MessageBox.Show($"File encrypted successfully! Encrypted file saved as: {saveFileDialog.FileName}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error during encryption: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private async void DecryptButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            string password = PasswordBox.Password;
+            string filePath = FilePathTextBox.Text;
+
+            // Patikrinimas, ar slaptažodis ir kelias nėra tuščias
+            if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Please enter both password and file path.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            byte[] encryptedData = await Task.Run(() => File.ReadAllBytes(filePath));
+            byte[] decryptedData = await Task.Run(() => dataEncryption.Decrypt(encryptedData, password));
+
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Decrypted Files|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                await Task.Run(() => File.WriteAllBytes(saveFileDialog.FileName, decryptedData));
+                MessageBox.Show($"File decrypted successfully! Decrypted file saved as: {saveFileDialog.FileName}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error during decryption: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
